a416027e7b4083fdea3472e64655939c3acab264
Martin Albert <martin.albert@st.othr.de>
2021-07-14 10:55:18 +0200

    Introducing SecOc

---
+++ scapy/contrib/automotive/secoc.py
@@ -0,0 +1,16 @@
+# This file is part of Scapy
+# See http://www.secdev.org/projects/scapy for more information
+# Copyright (C) Martin Albert <macool8@t-online.de>
+# This program is published under a GPLv2 license
+
+# This is an implementation for Scapy to communicate with devices which have SecOc implemented
+# SecOc is an AUTOSAR module described in the AUTOSAR document
+# "Specification of Secure Onboard Communication" (Doc. Id: 654)
+
+# scapy.contrib.description = Secure Onboard Communication (SecOc)
+# scapy.contrib.status = loads
+
+
+
+
+
